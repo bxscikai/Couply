0941707e895726ca89301328298a6c51f1f119bb
minetteyou <emmaminyu@gmail.com>
2015-03-08 14:13:17 -0400

    Added a second page

--- Couply/ViewController.swift
+++ Couply/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var recordingInProgress: UILabel!
    
 
+    @IBOutlet weak var stopButton: UIButton!
+    
+    @IBOutlet weak var recordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,18 +27,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
         // Add comment
-        // Chenkai is an asshole
-        
+    
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        stopButton.hidden=true
     }
 
     @IBAction func clickToRecord(sender: UIButton) {
         recordingInProgress.hidden=false
-       
-        
+        stopButton.hidden=false
+        recordButton.enabled=false
+        stopButton.enabled=true
     }
     
     @IBAction func stopRecording(sender: UIButton) {
         recordingInProgress.hidden=true
+        recordButton.enabled=true
+        stopButton.enabled=false
     }
   
   
